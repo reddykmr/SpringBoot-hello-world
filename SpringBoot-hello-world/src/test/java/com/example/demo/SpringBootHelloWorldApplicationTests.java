53576f2e7053928d8964f2c2b7104e411c722a5f
karna-mahesh <mahesh.karna@thermofisher.com>
2021-10-23 20:22:32 +0530

    new update

--- SpringBoot-hello-world/src/test/java/com/example/demo/SpringBootHelloWorldApplicationTests.java
+++ SpringBoot-hello-world/src/test/java/com/example/demo/SpringBootHelloWorldApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+
+import javax.annotation.security.RunAs;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @SpringBootTest
 class SpringBootHelloWorldApplicationTests {
 
-	@Test
-	void contextLoads() {
+	
+	@Mock
+	RestTemplate restTemplate;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		
 	}
+	
+	
+	@Test
+	public void test_showHello() {
+		   String msg="hello";
+		   ResponseEntity<String> responseEntity=new ResponseEntity<>(msg,HttpStatus.OK);
+		   doReturn(responseEntity).when(restTemplate).getForEntity(ArgumentMatchers.anyString(), ArgumentMatchers.any());
+		   assertNotNull(responseEntity.getBody());
+		   assertEquals(responseEntity.getStatusCodeValue(),200);
+		   	   
+		   
+	}
+	
+	
 
 }
